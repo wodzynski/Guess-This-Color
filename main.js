fab22d162933dc639f67f9db0befcdfcb89b71ae
wodzynski <macko5395@gmail.com>
2018-06-19 23:31:24 +0200

    At this point i understood it will be impossible to win XD

--- main.js
+++ main.js
@@ -1,43 +1,68 @@
-var rSelectors = document.querySelectorAll(".red");
-var gSelectors = document.querySelectorAll(".green");
-var bSelectors = document.querySelectorAll(".blue");
+var redCol = document.querySelectorAll(".red");
+var greenCol = document.querySelectorAll(".green");
+var blueCol = document.querySelectorAll(".blue");
+var selectors = [];
+selectors.push(redCol, greenCol, blueCol);
+var colors = [];
+var randReds = [];
+var randGreens = [];
+var randBlues = [];
+var solution = [];
+var h1 = document.querySelector("h1");
+var optionsNum = 3;
 
-var reds = [];
-var greens = [];
-var blues = [];
+init();
 
-getReds();
-getBlues();
-getGreens();
-
-for (let i = 0; i < 3; i++) {
-  rSelectors[i].style.backgroundColor = reds[i];
-  gSelectors[i].style.backgroundColor = greens[i];
-  bSelectors[i].style.backgroundColor = blues[i];
+function init() {
+  getColors();
+  setSquares();
+  createGoalColor();
 }
 
-function getReds() {
+function setSquares() {
   for (let i = 0; i < 3; i++) {
-    reds[i] = "rgb(" + rgbValue() + ", 0, 0)";
+    selectors[0][i].style.backgroundColor = colors[0][i];
+    selectors[1][i].style.backgroundColor = colors[1][i];
+    selectors[2][i].style.backgroundColor = colors[2][i];
   }
 }
 
-function getGreens() {
+function createGoalColor() {
+  pickSolution(optionsNum);
+  h1.style.backgroundColor =
+    "rgb(" +
+    randReds[solution[0]] +
+    ", " +
+    randGreens[solution[1]] +
+    ", " +
+    randReds[solution[2]] +
+    ")";
+}
+
+function getColors() {
+  rgbValues(randReds);
+  rgbValues(randGreens);
+  rgbValues(randBlues);
+  var r = [];
+  var g = [];
+  var b = [];
   for (let i = 0; i < 3; i++) {
-    greens[i] = "rgb(0, " + rgbValue() + ", 0)";
+    r.push("rgb(" + randReds[i] + ", 0, 0)");
+    g.push("rgb(0, " + randGreens[i] + ", 0)");
+    b.push("rgb(0, 0, " + randBlues[i] + ")");
+  }
+  colors.push(r, g, b);
+}
+
+function pickSolution(i) {
+  for (let index = 0; index < 3; index++) {
+    solution[index] = Math.floor(Math.random() * i);
   }
 }
 
-function getBlues() {
-  for (let i = 0; i < 3; i++) {
-    blues[i] = "rgb(0, 0, " + rgbValue() + ")";
+function rgbValues(color) {
+  for (let index = 0; index < 3; index++) {
+    color[index] = Math.floor(Math.random() * 256);
   }
-}
-
-function oneFromThree() {
-  return Math.floor(Math.random() * 3);
-}
-
-function rgbValue() {
-  return Math.floor(Math.random() * 256);
+  return color;
 }
